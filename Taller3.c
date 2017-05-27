dd46014a8d11aa31e1f318ccd3992032e227ff7d
Allan Alarcón <samuel.alarcon@outlook.com>
2017-05-27 15:07:56 -0500

    Primera versión de la función Cifrado Cíclico

--- Taller3.c
+++ Taller3.c
@@ -1,1 +1,23 @@
-#include
+#include <stdio.h>
+#include <ctype.h>
+
+char cifrado[200] = ""
+
+void cifradoCiclico(char mensaje[], int llave)
+{
+	char cadena[200] = ""
+	if (strlen(mensaje > 200)
+		printf("El mensaje no debe tener más de 200 caracteres");
+	else
+	{
+		for (int i = 0; i < strlen(mensaje); i++)
+		{
+			if (ispunct(mensaje[i]) || isspace(mensaje[i]))
+				cifrado[i] = mensaje[i];
+			else if (((mensaje[i] < 91) && ((mensaje[i] + llave) > 90) || ((mensaje[i] + llave) > 122))
+				cifrado[i] = mensaje[i] + llave - 26;
+			else
+				cifrado[i] = mensaje[i] + llave;
+		}
+	}
+}
