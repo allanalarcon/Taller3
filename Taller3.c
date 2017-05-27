5e58e6623f0de8327ba18755c996d965b4e9ca11
Allan Alarcón <samuel.alarcon@outlook.com>
2017-05-27 16:40:48 -0500

    Cifrado ciclico terminado

--- Taller3.c
+++ Taller3.c
@@ -1,23 +1,40 @@
 #include <stdio.h>
+#include <string.h>
 #include <ctype.h>
 
-char cifrado[200] = ""
+char cifrado[1024] = "";
 
 void cifradoCiclico(char mensaje[], int llave)
 {
-	char cadena[200] = ""
-	if (strlen(mensaje > 200)
-		printf("El mensaje no debe tener más de 200 caracteres");
+	char cadena[1024] = "";
+	if (strlen(mensaje) > 1024)
+		printf("El mensaje no debe tener más de 1024 caracteres");
 	else
 	{
 		for (int i = 0; i < strlen(mensaje); i++)
 		{
 			if (ispunct(mensaje[i]) || isspace(mensaje[i]))
 				cifrado[i] = mensaje[i];
-			else if (((mensaje[i] < 91) && ((mensaje[i] + llave) > 90) || ((mensaje[i] + llave) > 122))
-				cifrado[i] = mensaje[i] + llave - 26;
+			else if ((mensaje[i] < 91) && ((mensaje[i] + llave) > 90))
+				cifrado[i] = mensaje[i] + llave - (26*((mensaje[i] + llave - 65)/26));
+			else if ((mensaje[i] > 96) && ((mensaje[i] + llave) > 122))
+				cifrado[i] = mensaje[i] + llave - (26*((mensaje[i] + llave - 97)/26));
 			else
 				cifrado[i] = mensaje[i] + llave;
+			printf("%c", cifrado[i]);
 		}
 	}
+}
+
+void main(void)
+{
+	char mensaje[1024];
+	int llave;
+	puts("CIFRADO CÍCLICO");
+  	printf("Ingrese mensaje a cifrar: ");
+	fgets(mensaje, 1024, stdin);
+	printf("Ingrese la llave numérica: ");
+        scanf("%d", &llave);
+	printf("Mensaje cifrado: ");
+	cifradoCiclico(mensaje, llave);
 }
