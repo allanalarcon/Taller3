c8b39af1cfad685d417a15dc2041bf9fa1bd146d
Irene Cordova <irene_cordova@outlook.es>
2017-06-03 13:58:07 -0500

    Creación de las funciones

--- include/cifrado.h
+++ include/cifrado.h
@@ -1,1 +1,3 @@
-fsd
+char* cifradoCiclico(char* mensaje, int llave);
+char* cifradoAutoLlave(char* mensaje, char* llave);
+char* cifradoContraseña(char* mensaje, char* llave);
--- src/main.c
+++ src/main.c
@@ -1,1 +1,38 @@
-dfdf
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+void main(int argc, char *argv[])
+{
+        if (argc != 3 && argc != 1)
+        {
+                printf("Debe ingresar la llave y el texto\n");
+        }
+        else if (argc == 3)
+        {
+                printf("Mensaje cifrado: ");
+                cifradoCiclico(argv[2], atoi(argv[1]));
+                printf("\n");
+                printf("Mensaje cifrado en morse: ");
+                printf("\n");
+        }
+        else
+        {
+                char mensaje[1024];
+                int llave;
+                char llave2[1024];
+                puts("CIFRADO CÍCLICO");
+                printf("Ingrese mensaje a cifrar: ");
+                fgets(mensaje, 1024, stdin);
+                printf("Ingrese la llave numérica: ");
+                scanf("%d", &llave);
+                printf("Mensaje cifrado: %s", cifradoCiclico(mensaje, llave));
+                memset(cifrado,0,1024);
+                printf("Ingrese la llave: ");
+                scanf("%s", llave2);
+                printf("Mensaje cifrado: %s", cifradoAutoLlave(mensaje, llave2));
+        }
+}
+
