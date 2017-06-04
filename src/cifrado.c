a8c81070a51a5c40f9fd16c22e4d28d0c1b5a5a9
Allan Alarcón <samuel.alarcon@outlook.com>
2017-06-03 20:08:59 -0500

    Actualización implementaciones

--- src/cifrado.c
+++ src/cifrado.c
@@ -3,43 +3,54 @@
 #include <ctype.h>
 #include <stdlib.h>
 
-char cifrado[1024] = "";
-
-char* cifradoCiclico(char* mensaje, int llave){
-        for (int i = 0; i < strlen(mensaje); i++)
-        {
-                if (ispunct(mensaje[i]) || isspace(mensaje[i]))
-                        cifrado[i] = mensaje[i];
-                else{
-                        cifrado[i] = mensaje[i] + llave;
-                        if(llave>=0){
-                                if(mensaje[i]>=65 && mensaje[i]<=90){
-                                        while(cifrado[i]<65 || cifrado[i]>90){
-                                                cifrado[i] = cifrado[i] - 26;
-                                        }
-                                }
-                                if(mensaje[i]>=97 && mensaje[i]<=122){
-                                        while(cifrado[i]<97 || cifrado[i]>122){
-                                                cifrado[i] = cifrado[i] - 26;
-                                        }
-                                }
-                        }
-                        else{
-                                if(mensaje[i]>=65 && mensaje[i]<=90){
-                                        while(cifrado[i]<65 || cifrado[i]>90){
-                                                cifrado[i] = cifrado[i] + 26;
-                                        }
-                                }
-                                if(mensaje[i]>=97 && mensaje[i]<=122){
-                                        while(cifrado[i]<97 || cifrado[i]>122){
-                                                cifrado[i] = cifrado[i] + 26;
-                                        }
-                                }
-                        }
-                }
-        }
-        //printf("%c", cifrado[i]);
-        return cifrado;
+char *cifradoCiclico(char *mensaje, int llave)
+{
+	char cifrado[1024] = "";
+	for (int i = 0; i < strlen(mensaje); i++)
+	{
+		if (ispunct(mensaje[i]) || isspace(mensaje[i]))
+			cifrado[i] = mensaje[i];
+		else
+		{
+			cifrado[i] = mensaje[i] + llave;
+			if(llave >= 0)
+			{
+				if(mensaje[i] >= 65 && mensaje[i] <= 90)
+				{
+					while(cifrado[i] < 65 || cifrado[i] > 90)
+					{
+						cifrado[i] = cifrado[i] - 26;
+					}
+				}
+				if(mensaje[i] >= 97 && mensaje[i] <= 122)
+				{
+					while(cifrado[i] < 97 || cifrado[i] > 122)
+					{
+						cifrado[i] = cifrado[i] - 26;
+					}
+				}
+			}
+			else
+			{
+				if(mensaje[i] >= 65 && mensaje[i] <= 90)
+				{
+					while(cifrado[i] < 65 || cifrado[i] > 90)
+					{
+						cifrado[i] = cifrado[i] + 26;
+					}
+				}
+				if(mensaje[i] >= 97 && mensaje[i] <= 122)
+				{
+					while(cifrado[i] < 97 || cifrado[i] > 122)
+					{
+						cifrado[i] = cifrado[i] + 26;
+					}
+				}
+			}
+		}
+	}
+	char *cif = cifrado;
+	return cif;
 }
 
 
--- src/codificacion.c
+++ src/codificacion.c
@@ -1,1 +1,18 @@
-d
+#include <stdio.h>
+#include "codificacion.h"
+
+char codMorse[30][5]={".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--",
+		      "-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+
+char *claveMorse(char *mensaje)
+{
+	for (int i = 0; i < strlen(mensaje); i++)
+	{
+		if ispunct(mensaje[i])
+			printf("%c ", mensaje[i]);
+		else if isspace(mensaje[i])
+			printf("/ ");
+		else
+			printf("%s ", codMorse[toupper(mensaje[i]) - 65]);
+	}
+}
